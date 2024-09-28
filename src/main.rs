d37c2bba9f091a14e8869e76c56d09d1c1e41024
Riku Ruokonen <riku.ruokonen@outlook.com>
2024-09-28 20:14:21 +0300

    Added last step of key-generation and encrypt/decrypt functionality

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,66 @@
 mod primes;
 mod math;
 
-use num::{bigint::{ToBigInt}};
+use base64::{engine::general_purpose, Engine as _};
+use num::{bigint::ToBigInt, BigInt};
+use num_bigint::Sign;
 use primes::Bint;
+use core::str;
 use std::env;
 
 
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    let (p1, p2) = primes::get_primes();
-    println!("Found two primes: {p1} and  {p2}");
-    let n = p1.clone() * p2.clone();
-    println!("n is: {n}");
-    let totient = math::euler_totient(p1.clone(), p2.clone());
-    println!("totient is: {totient}");
-    let e = Bint!(65537);
-    let d = math::modular_multip_inverse(e, totient);
+    let (public_key, private_key) = generate_key_pair();
+    println!("Public Key: {:?}", public_key);
+    println!("Private Key: {:?}", private_key);
+    let original_message = "Test String";
+    // Encrypt the message
+    let encrypted_message = encrypt(&original_message, public_key);
+    println!("Encyrpted message: {encrypted_message}");
+    let decrypted_message = decrypt(&encrypted_message, private_key);
+    println!("Decyrpted message: {decrypted_message}");
+
 }
 
+fn construct_public_key(n: BigInt, e: BigInt) -> (BigInt, BigInt) {
+    return (n, e)
+}
+
+fn construct_private_key(n: BigInt, d: BigInt) -> (BigInt, BigInt) {
+    return (n, d)
+}
+
+fn generate_key_pair() -> ((BigInt, BigInt), (BigInt, BigInt)){
+    let (p1, p2) = primes::get_primes();
+    let n = p1.clone() * p2.clone();
+    let totient = math::euler_totient(p1.clone(), p2.clone());
+    let e = Bint!(65537);
+    let d = math::modular_multip_inverse(e.clone(), totient);
+    let public_key = construct_public_key(n.clone(), e.clone());
+    let private_key = construct_private_key(n.clone(), d.clone());
+    return (public_key, private_key)
+}
+
+
+pub fn encrypt(message: &str, public_key: (BigInt, BigInt)) -> String {
+    //TODO: Consider using BigUint across application.
+    let encoded = BigInt::from_bytes_be(Sign::Plus, message.as_bytes());
+    //Encrypt via modpow by using public key parts as exponent and modulus
+    let encrypted = encoded.modpow(&public_key.1, &public_key.0);
+    let encrypted_bytes = encrypted.to_bytes_be().1;
+    let encrypted_base64 = general_purpose::STANDARD.encode(encrypted_bytes);
+    return encrypted_base64
+}
+
+pub fn decrypt(ciphertext: &str, private_key: (BigInt, BigInt)) -> String {
+    let decoded_bytes = general_purpose::STANDARD.decode(ciphertext).expect("Invalid Base64 conversion");
+    let decoded = BigInt::from_bytes_be(Sign::Plus, &decoded_bytes);
+    let decrypted = decoded.modpow(&private_key.1, &private_key.0);
+    let decrypted_bytes = decrypted.to_bytes_be().1;
+    return String::from_utf8(decrypted_bytes).expect("Invalid UTF-8 byte-representation");
+}
 
 
 //Initial tests for prime-gen checking
@@ -38,5 +80,13 @@ mod tests {
             let is_p = primes::is_prime(&primes::Bint!(non_prime));
             assert_eq!(is_p, false);
          }
+    }
+    #[test]
+    fn test_encryption_decryption() {
+        let (public_key, private_key) = generate_key_pair();
+        let original_message = "Algolabra Test String";
+        let encrypted_message = encrypt(original_message, public_key);
+        let decrypted_message = decrypt(&encrypted_message, private_key);
+        assert_eq!(original_message, decrypted_message, "The decrypted message does not match the original one.");
     }
 }
--- src/math.rs
+++ src/math.rs
@@ -1,10 +1,47 @@
-use num::BigInt;
+use num::{one, BigInt, One, Zero};
 
 pub fn euler_totient(q: BigInt, p: BigInt) -> BigInt {
     let result: BigInt = (q-1) * (p-1);
     return result
 }
 
-pub fn modular_multip_inverse(e: BigInt, totient: BigInt) {
- //TODO: Implement this last step of key generation
+// Find (e⋅d)≡1 (mod totient) with extended euclidean algorithm
+pub fn modular_multip_inverse(e_param: BigInt, totient_param: BigInt) -> BigInt {
+    let mut s: BigInt = BigInt::zero();
+    //This old_s will house modular inverso 
+    let mut old_s = BigInt::one();
+    let mut t: BigInt = BigInt::one();
+    let mut old_t = BigInt::zero();
+    let mut r: BigInt = totient_param.clone();
+    let mut old_r = e_param.clone();
+
+    while r > BigInt::zero() {
+        let quotient = old_r.clone() / r.clone();
+
+        //Tuple switch:  (old_r, r) <- (r, old_r - quotient * r)
+        let temp_r = r.clone();
+        r = old_r.clone() - quotient.clone() * r.clone();
+        old_r = temp_r;
+
+        //Similar tuple-switch for s
+        let temp_s = s.clone();
+        s = old_s.clone() - quotient.clone() * s.clone(); 
+        old_s = temp_s;
+
+        //Similar tuple-switch for t
+        let temp_t = t.clone();
+        t = old_t.clone() - quotient.clone() * t.clone(); 
+        old_t = temp_t;
+    }
+
+    // Ensure the result is positive
+    let result = old_s % &totient_param;
+    // If the result is negative, perform adjustment by adding totient-param
+    if result < BigInt::zero() {
+        return result + &totient_param;
+    }
+    result
+
+
 }
+
